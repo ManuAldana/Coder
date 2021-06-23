3a52d032313e30a250d7171c23eb06551ebb8dac
Manuel Aldana <mnl.ald@gmail.com>
2021-06-22 23:28:23 -0300

    Entrega desafío Componentes II

---
+++ src/Components/itemCount/itemCount.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+export const ItemCount = ({stock, inicial}) => {
+    const [capacidad, setCapacidad] = useState(stock)
+    const [number, setNumber] = useState(inicial)
+
+    const resultadoOperacion = (operator) => {
+        let nuevoResultado
+        let nuevoStock
+
+        
+        switch (operator){
+            case '+':
+                if (capacidad < 1 ){
+                    return(alert('Stock agotado'))
+                }
+                else{
+                    nuevoStock = Number(capacidad) - 1
+                    nuevoResultado = Number(number) + 1
+                }
+            break
+            case '-':
+                if (number < 1){
+                    return(alert('No se pueden seleccionar menos valores'))
+                }
+                else{
+                    nuevoResultado = Number(number) - 1
+                    nuevoStock = Number(capacidad) + 1
+                }
+            break
+        }
+        setNumber (nuevoResultado)
+        setCapacidad (nuevoStock)
+    }
+    
+    return(
+        <div className='itemCount'>
+            <label htmlFor='number'>
+                Item Seleccionado:
+                   <input
+                        id='number'
+                        type='number'
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                   />
+            </label>
+            <div>
+                <button onClick={() => resultadoOperacion('-')}>-</button>
+                <button onClick={() => resultadoOperacion('+')}>+</button>
+            </div>
+        </div>
+    )
+}
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,12 +1,14 @@
 import './home.css'
 import { NavBar } from '../Components/navBar/navBar'
 import { ItemListContainer } from '../Components/itemListContainer/itemListContainer'
+import { ItemCount } from '../Components/itemCount/itemCount'
 
 export const Home = () => {
     return (
         <div>
             <NavBar />
             <ItemListContainer greeting="Hola" />
+            <ItemCount stock="5" inicial="1" />
         </div>
     )
 }
